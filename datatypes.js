655c41aa252cb48481dd726354dc0fe1d75acdd6
minghl <a879307253@gmail.com>
2023-09-10 18:24:45 +0200

    01 TS - Data Types

--- datatypes.js
+++ datatypes.js
@@ -11,4 +11,26 @@ let dob = '25';
 let restul = parseInt(dob);
 let isValid = false;
 console.log(isValid);
-// lname = 10;
+// two ways to define the array in typescript
+let empList;
+empList = ["Santosh", "Santosh1", "Santosh2"];
+let numList;
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// let newNum = numList[5];
+let result = numList.filter((num) => num > 2);
+let emp = empList.find((emp) => emp === 'Santosh');
+let sum = numList.reduce((acc, num) => acc + num);
+console.log(result);
+console.log(emp);
+console.log(sum);
+let c = 2 /* Color.Blue */;
+let swapNumbs;
+function swapNumbers(num1, num2) {
+    return [num2, num1];
+}
+swapNumbs = swapNumbers(10, 20);
+swapNumbs[0];
+swapNumbs[1];
+let department;
+department = 'IT';
+department = 10;
--- datatypes.ts
+++ datatypes.ts
@@ -20,5 +20,52 @@ let isValid : boolean =false;
 
 console.log(isValid);
 
+// two ways to define the array in typescript
+let empList : string[];
+
+empList = ["Santosh", "Santosh1", "Santosh2"];
+
+
+let numList : Array<number>;
+
+numList = [1,2,3,4,5,6,7,8,9,10,11];
+
+// let newNum = numList[5];
+
+let result = numList.filter((num) => num>2);
+
+let emp = empList.find((emp) => emp==='Santosh');
+
+let sum = numList.reduce((acc, num) => acc + num);
+
+console.log(result);
+console.log(emp);
+console.log(sum);
 
 // lname = 10;
+
+
+const enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+let c : Color = Color.Blue;
+
+let swapNumbs: [number, number];
+
+function swapNumbers(num1: number,num2: number) : [number, number] {
+    return [num2, num1];
+}
+
+swapNumbs = swapNumbers(10,20);
+
+swapNumbs[0];
+swapNumbs[1];
+
+let department: any;
+
+department = 'IT';
+
+department = 10;
